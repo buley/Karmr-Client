ab2c713899b263cedd0e647f1f18d432e1e9cbf7
Taylor Buley <taylor.buley@gmail.com>
2012-06-27 00:57:56 -0400

    ...

--- karmr.js
+++ karmr.js
@@ -1,51 +1,46 @@
 ( function( Karmr ) { 
 
-	$( document ).ready( function( e ) {
+    Karmr.websocket = Karmr.websocket || {};
+    Karmr.protocol = 'http';
+    Karmr.domain = 'karmr.herokuapp.com';
+    Karmr.accounts = null;
+    Karmr.socket = io.connect( Karmr.protocol + '://' + Karmr.domain );
 
-	    Karmr.websocket = Karmr.websocket || {};
-	    Karmr.protocol = 'http';
-	    Karmr.domain = 'karmr.herokuapp.com';
-	    Karmr.accounts = null;
-	    Karmr.socket = io.connect( Karmr.protocol + '://' + Karmr.domain );
+    /* Karmr */
 
-	    /* Karmr */
-
-		Karmr.accounts.subscribe( 'sessioning', function( data ) {
-			console.log("SESSIONING",data)
-		} );
-
-
-		Karmr.accounts.subscribe( 'sessioned', function( data ) {
-			console.log("SESSIONING",data)
-		    Karmr.profiles.update();
-		} );
-
-		Karmr.accounts.subscribe( 'unsessioned', function( data ) {
-			console.log("unsessioned",data)
-		    Karmr.profiles.update();
-		} );
-
-		Karmr.accounts.subscribe( 'profile', function( data ) {
-			console.log("PROFILE",data);
-		} );
-
-		/* Socket */
-
-		Karmr.socket.on('api',function(data) {
-			console.log("api",JSON.stringify(data));
-		});
-
-		Karmr.socket.on('connect',function(data) {
-			console.log("CONNECTED",data);
-		    Karmr.accounts = new Accounts( Karmr.socket );
-		});
-
-		Karmr.socket.on('disconnect',function() {
-			console.log("DISCONNECTED");
-		});
-
-
-
+	Karmr.accounts.subscribe( 'sessioning', function( data ) {
+		console.log("SESSIONING",data)
 	} );
 
-}( Karmr ) );
+
+	Karmr.accounts.subscribe( 'sessioned', function( data ) {
+		console.log("SESSIONING",data)
+	    Karmr.profiles.update();
+	} );
+
+	Karmr.accounts.subscribe( 'unsessioned', function( data ) {
+		console.log("unsessioned",data)
+	    Karmr.profiles.update();
+	} );
+
+	Karmr.accounts.subscribe( 'profile', function( data ) {
+		console.log("PROFILE",data);
+	} );
+
+	/* Socket */
+
+	Karmr.socket.on('api',function(data) {
+		console.log("api",JSON.stringify(data));
+	});
+
+	Karmr.socket.on('connect',function(data) {
+		console.log("CONNECTED",data);
+	    Karmr.accounts = new Accounts( Karmr.socket );
+	});
+
+	Karmr.socket.on('disconnect',function() {
+		console.log("DISCONNECTED");
+	});
+
+
+}( Karmr || {} ) );
