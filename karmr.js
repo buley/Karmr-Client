3c7cce837330e68c716d37d8f6593b158be381cf
Taylor Buley <taylor.buley@gmail.com>
2012-06-26 22:33:14 -0400

    ...

--- karmr.js
+++ karmr.js
@@ -1,36 +1,43 @@
 var Karmr = Karmr || {};
 
 $( document ).ready( function( e ) {
+	
     Karmr.websocket = Karmr.websocket || {};
     Karmr.domain = 'api.Karmr.com';
     Karmr.socket = io.connect( 'https://' + Karmr.domain );
     Karmr.accounts = new Accounts( Karmr.socket );
+
+    /* Karmr */
+
+	Karmr.accounts.subscribe( 'sessioned', function( data ) {
+	    Karmr.profiles.update();
+	} );
+
+	Karmr.accounts.subscribe( 'unsessioned', function( data ) {
+	    Karmr.profiles.update();
+	} );
+
+	Karmr.accounts.subscribe( 'profile', function( data ) {
+		console.log("PROFILE",data);
+	} );
+
+	/* Socket */
+
+	Karmr.socket.on('api',function(data) {
+		console.log("api",JSON.stringify(data));
+	});
+
+	Karmr.socket.on('connect',function() {
+		console.log("CONNECTED");
+	});
+
+	Karmr.socket.on('disconnect',function() {
+		console.log("DISCONNECTED");
+	});
+
+
+
 } );
 
-
-Karmr.accounts.subscribe( 'sessioned', function( data ) {
-    Karmr.profiles.update();
-} );
-
-Karmr.accounts.subscribe( 'unsessioned', function( data ) {
-    Karmr.profiles.update();
-} );
-
-
-Karmr.accounts.subscribe( 'profile', function( data ) {
-	console.log("PROFILE",data);
-} );
-
-Karmr.socket.on('api',function(data) {
-	console.log("api",JSON.stringify(data));
-});
-
-Karmr.socket.on('connect',function() {
-	console.log("CONNECTED");
-});
-
-Karmr.socket.on('disconnect',function() {
-	console.log("DISCONNECTED");
-});
 
 
